b9da9a9e8011b8ad82d3b7125bd35861d5dc36e7
Nick Pantev <103816194+IYI-Pantev@users.noreply.github.com>
2025-06-25 19:49:30 +0300

    project config

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className=" flex flex-col items-center justify-start p-6 space-y-8">
+      <h1 className="text-4xl font-bold text-white text-center mt-5">
+        Infinite possibilities
+      </h1>
+    </div>
+  );
+}
+
+export default App;
